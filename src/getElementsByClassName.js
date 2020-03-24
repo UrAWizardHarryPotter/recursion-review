b0f36121a609817f836ff15170d06acb2c0be8bf
UrAWizardHarryPotter <zach.schwartzman@gmail.com>
2020-03-23 17:37:33 -0700

    Add first part of code for stringifyJSON and getElementsByclassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,14 +5,29 @@
 
 // But instead we're going to implement it from scratch:
 
+
+
+
+
 var getElementsByClassName = function(className) {
-  var parent = document.body;
+  // input is a class name
+  // assign an empty array
+  var resultArr = [];
 
-  var expectedNodeList = document.getElementsByClassName('targetClassName');
-  var expectedArray = Array.prototype.slice.apply(expectedNodeList);
-  console.log(expectedArray);
-
+  var recursiveInnerFunction = function(className) {
+    // iterate through the document.body
+    for (var i = 0; i < sheet.children.length; i++) {
+      // if grandDiv[i].classList === className
+      if (sheet.children[i].classList === className) {
+        // reassign grandDiv to grandDiv[i]
+        sheet.children = sheet.children[i];
+        // push grandDiv[i] into resultArray
+        resultArr.push(sheet.children);
+        // invoke recursive inner Function with grandDiv[i] argument
+        recursiveInnerFunction(sheet.children.classList);
+      }
+    }
+  };
+  return resultArr;
 };
-
-getElementsByClassName();
 
